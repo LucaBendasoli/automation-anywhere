28b38a0d0ed42f4fbbb56ca4cc1ce4b547641bc8
LucaAgostinho <lucaabenda@icloud.com>
2025-02-18 23:49:21 -0300

    chore: update module docstring and readability

--- bot-games/1_basic_login/config/config.py
+++ bot-games/1_basic_login/config/config.py
@@ -1,5 +1,3 @@
-"""Este arquivo irá conter variáveis importantes que serão importadas para a execução do código principal."""
+"""This module contains core variables for the exercise."""
 
-URL = "https://pathfinder.automationanywhere.com/challenges/AutomationAnywhereLabs-Login.html?_gl=1*1hchg56*_gcl_au*ODc5NjQ2NDEwLjE3Mzk4NDg1MDI.*_ga*MTc3NzI0MTUwMi4xNzM5ODQ4NTAz*_ga_DG1BTLENXK*MTczOTg0ODUwMi4xLjAuMTczOTg0ODUwNS41Ny4wLjE0NjE5MTYwMDg."
-
-# driverk
+CHALLANGE_URL = "https://pathfinder.automationanywhere.com/challenges/AutomationAnywhereLabs-Login.html?_gl=1*1hchg56*_gcl_au*ODc5NjQ2NDEwLjE3Mzk4NDg1MDI.*_ga*MTc3NzI0MTUwMi4xNzM5ODQ4NTAz*_ga_DG1BTLENXK*MTczOTg0ODUwMi4xLjAuMTczOTg0ODUwNS41Ny4wLjE0NjE5MTYwMDg."
