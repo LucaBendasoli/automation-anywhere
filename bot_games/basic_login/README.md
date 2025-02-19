3592628ae043bc226f7fed019f14f71fc5ec8216
LucaAgostinho <lucaabenda@icloud.com>
2025-02-18 23:56:09 -0300

    refactor: change folders nomenclature

--- bot_games/basic_login/README.md
+++ bot_games/basic_login/README.md
@@ -1,10 +1,10 @@
 # Basic Login
 
-Primeiro exercício na seção bot games da automation anywhere.
+First automation anywhere bot games challange.
 
 ---
 
-## Visão Geral do Exercício
+## Review
 
 A great place to start for anyone new to Automation Anywhere development. Many web applications
 used by bots require some sort of login - in this challenge page, developers are asked to do
@@ -19,7 +19,7 @@ Password: Automation123
 
 ---
 
-- *link para o site do login:*
+***WebSite:***
 
 \```https://pathfinder.automationanywhere.com/challenges/AutomationAnywhereLabs-Login.html?_gl=1*1hchg56*_gcl_au*ODc5NjQ2NDEwLjE3Mzk4NDg1MDI*_ga*MTc3NzI0MTUwMi4xNzM5ODQ4NTAz*_ga_DG1BTLENXK*MTczOTg0ODUwMi4xLjAuMTczOTg0ODUwNS41Ny4wLjE0NjE5MTYwMDg.
 \```
--- bot_games/basic_login/main.py
+++ bot_games/basic_login/main.py
@@ -1,16 +1,20 @@
 """Este é o arquivo principal da solução do exercício "Basic Login" do automation anywhere bot games. """
 
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from config.config import URL
+from config.config import CHALLANGE_URL
+
+load_dotenv('.env')
 
 driver = webdriver.Chrome()
-driver.get(URL)
+driver.get(CHALLANGE_URL)
 driver.find_element(By.XPATH, '//*[contains(text(), "Aceitar cookies")]').click()
 driver.find_element(By.XPATH, '//*[@aria-label="Community login"]').click()
-driver.find_element(By.XPATH, '//*[@placeholder="*Email"]').send_keys("user@automationanywhere.com")
+driver.find_element(By.XPATH, '//*[@placeholder="*Email"]').send_keys(os.getenv('USERNAME'))
 driver.find_element(By.XPATH, '//*[@placeholder="*Email"]/../../following-sibling::div[1]').click()
-driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("Automation123")
+driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys(os.getenv('PASSWORD'))
 driver.find_element(By.XPATH, "//input[@placeholder='Password']/../../following-sibling::div[1]/div/div/button").click()
